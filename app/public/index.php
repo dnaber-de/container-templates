990642ae4e4970cfb2b9ae769d2a6a574d070200
David Naber <kontakt@dnaber.de>
2020-02-12 09:45:42 +0100

    Add IP info to index.php

--- app/public/index.php
+++ app/public/index.php
@@ -3,5 +3,8 @@ declare(strict_types=1);
 
 require_once dirname(__DIR__) . '/vendor/autoload.php';
 
-echo '<h1>Container works!</h1>';
-
+echo '<h1>Hello container visitor</h1>';
+echo "<p>
+Container IP: {$_SERVER['SERVER_ADDR']}<br>
+Client IP: {$_SERVER['REMOTE_ADDR']}
+</p>";
